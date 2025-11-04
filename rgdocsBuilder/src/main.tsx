0346de2f4b4095b966fdccebdb8e6bfc55a8ae69
wasubu <delmarmendoza41@gmail.com>
2025-11-04 16:35:30 +0900

    I added todo list so I wont forget

--- rgdocsBuilder/src/main.tsx
+++ rgdocsBuilder/src/main.tsx
@@ -1,3 +1,13 @@
+/*
+todos:
+[ ] - hamburg menu breaking on smaller screens
+[ ] - make the sidebar be able to open and close
+[ ] - fix the folder structure of cpu and reality chip
+[ ] - rename Modules to Misc Modules
+[ ] - customize the scrollbar https://preline.co/docs/custom-scrollbar.html
+[ ] - bento grid module in welcome!
+*/
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
