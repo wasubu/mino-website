f8861f674847c6bb6079a25477130a68e743bbb5
wasubu <delmarmendoza41@gmail.com>
2025-11-07 11:34:44 +0900

    updated todo, fixed the very small shadow on sidebar, run build. added meta data for seo

--- rgdocsBuilder/src/main.tsx
+++ rgdocsBuilder/src/main.tsx
@@ -5,21 +5,31 @@ todos:
 [x] - hamburg menu breaking on smaller screens
 [x] - make the sidebar be able to open and close
 [x] - make Input modules independent from Misc Modules
+[x] - maybe on mobile the sidebar is just a pop up over the main
+[x] - blur background on smaller screens when openning the sidebar
+[x] - fix dimming when openning the sidebar on smaller screens
+[x] - if sidebar closed the sidebars text will turn gray
+[x] - make navbar elements not able to be selected
+[x] - add metadata for seo
+[x] - eliminate the very small shadow on the sidebar
 [ ] - populate the sidebar
-[ ] - make navbar elements not able to be selected
-[ ] - make a custom breakpoint, after-breakpoint and before-breakpoint
-[ ] - blur background on smaller screens when openning the sidebar
+[ ] - make a example json for storing all routes off pages
 [ ] - customize the scrollbar of sidebar https://preline.co/docs/custom-scrollbar.html
 [ ] - modules page
-[ ] - if sidebar closed the sidebars text will turn gray
+[ ] - openning the sidebar would shift the main content slightly
+[ ] - new sidebar tab where you can search
 [ ] - bento grid module in welcome!
+[ ] - lua will be included in the docs, luau too
+[ ] - docutmentRoutes.json contains each element. element has to: name: component:
+[ ] - don't use canvas or pixijs, just use react for interactive components
+[ ] - main screens contents sizes based of the parent not the viewport Container queries
 [ ] - work on the footer
 [ ] - include every module on the sidebar
+[ ] - smaller screens, clicking an item in the sidebar would close it
 [ ] - no videos/gif but overing on them plays an animation, smooth in and out 
+[ ] - make a custom breakpoint, after-breakpoint and before-breakpoint
 [ ] - Cpu and other modules say gdt.Cpu0
-[ ] - maybe on mobile the sidebar is just a pop up over the main
 [ ] - make small images have smaller resolution for faster loading
-[ ] - add metadata for seo
 [ ] - change icon to my icon
 [ ] - hear me out. Module icon in the left and on the right it says like codeblock update() gdt.Slider0.Value == 100, and sliding the slider would change the value 
 [ ] - on the container there would be a power button so, cpu.sincelastreload would reset. the button could play a meow sound if buttondown.
