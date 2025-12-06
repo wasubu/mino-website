9e61365bcf639f9ba5304070f3f8e00ce085250d
wasubu <delmarmendoza41@gmail.com>
2025-12-06 15:46:50 +0900

    navbar has it's own height tailwind variable

--- rgdocsBuilder/src/components/Navbar.tsx
+++ rgdocsBuilder/src/components/Navbar.tsx
@@ -5,7 +5,7 @@ import rgDocsLogo from "../assets/docsSmLogo.webp"
 const Navbar: React.FC<{ toggleSidebar: () => void }> = ({ toggleSidebar }) => {
     return (
         <header className="select-none sticky top-0 z-50 bg-white text-black shadow-sm">
-            <div className="flex justify-between items-center h-[46px]">
+            <div className="flex justify-between items-center h-navY">
                 <div onClick={toggleSidebar} className="hover:cursor-pointer hidden sm:flex items-center px-4.5 gap-1.5 hover:bg-gray-200 transition-colors h-full">
                     <img src={rgDocsLogo} className="h-5.5" alt="rgDocs" />
                     <h1 className="text-[21px] font-semibold italic">RGdocs</h1>
@@ -16,7 +16,7 @@ const Navbar: React.FC<{ toggleSidebar: () => void }> = ({ toggleSidebar }) => {
                 <div onClick={toggleSidebar} className="hover:bg-gray-200 transition-colors rounded-full mx-1.5 p-1.5">
                     <img src={hamburgSVG} className="sm:hidden block h-6" alt="hamburg" />
                 </div>
-                <div className="flex items-center h-[46px] px-3 hover:bg-gray-200 transition-colors cursor-pointer" onClick={() => window.location.href = "https://www.wasubu.com"}>
+                <div className="flex items-center h-navY px-3 hover:bg-gray-200 transition-colors cursor-pointer" onClick={() => window.location.href = "https://www.wasubu.com"}>
                     <h1 className="text-[20px] mr-2 leading-none">Wasubu Minoco</h1>
                     <img src={myIcon} alt="myIcon" className="h-7 w-7 rounded-full object-cover" />
                 </div>
--- rgdocsBuilder/src/pages/Experiment.tsx
+++ rgdocsBuilder/src/pages/Experiment.tsx
@@ -1,10 +1,18 @@
-
+//current file name Experiment.tsx
 const Experiment: React.FC = () => {
+    const pageStyle = (
+        `min-h-[calc(100vh-var(--spacing-navY)-15px)] py-5 px-8 shadow-sm
+        border-2 rounded-2xl border-gray-200 m-2 flex flex-col`
+    )
     return (
-        <div className="py-5 px-8 shadow-sm flex flex-col min-h-[calc(93vh)] border-2 rounded-2xl border-gray-200 m-2">
+        <div className={pageStyle}>
             <h2 className="text-2xl font-bold">Experiment</h2>
         </div>
     )
 }
-
 export default Experiment;
+
+
+
+
+
